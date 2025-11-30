70b5744b6a6d3d56108cc60135dfa77d5a0d3522
Rasyid Oktavian <rasyidokt05@gmail.com>
2025-12-01 01:50:06 +0700

    fix: Import CustomerController in web routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\QRController;
+use App\Http\Controllers\CustomerController;
 
 // ==================== AUTHENTICATION ROUTES ====================
 
